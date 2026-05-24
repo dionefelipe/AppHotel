44629c3bbbc29a5e08ed10343df782d1b2fd0445
dionefelipe <dionefelipe@gmail.com>
2026-05-23 21:54:09 -0300

    Agenda 14

--- AppHotel/App.xaml.cs
+++ AppHotel/App.xaml.cs
@@ -1,10 +1,20 @@
-﻿using Microsoft.Maui;
+﻿using AppHotel.Models;
+using Microsoft.Maui;
 using Microsoft.Maui.Controls;
+
 
 namespace AppHotel
 {
     public partial class App : Application
     {
+        public List<Quarto> lista_quartos = new List<Quarto>
+        {
+            new Quarto() { Descricao = "Suite Super Luxo", ValorDiariaAdulto = 110, ValorDiariaCrianca = 55 },
+            new Quarto() { Descricao = "Suite Luxo", ValorDiariaAdulto = 80, ValorDiariaCrianca = 40 },
+            new Quarto() { Descricao = "Suite Single", ValorDiariaAdulto = 50, ValorDiariaCrianca = 25 },
+            new Quarto() { Descricao = "Suite Crise", ValorDiariaAdulto = 25, ValorDiariaCrianca = 12.5 }
+        };
+        
         public App()
         {
             InitializeComponent();
